8435251f7c3859b901242f5a13fe131c1214b652
akakitani <atsunori.kakitani@gmail.com>
2014-03-08 21:24:35 -0800

    Set up project structure for heroku

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Flask
+from flask import render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/robots.txt')
+def robots():
+    res = app.make_response('User-agent: *\nAllow: /')
+    res.mimetype = 'text/plain'
+    return res
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
