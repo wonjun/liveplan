ef881cfb7a000d59e6b3d60a4e2ef1ef8587bf78
akakitani <atsunori.kakitani@gmail.com>
2014-03-08 22:46:15 -0800

    Add database setup logic

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from models import *
 
+from database import db_session
+
 IS_HEROKU = 'IS_HEROKU' in os.environ
 
 app = Flask(__name__)
@@ -16,7 +18,7 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def admin_dashboard():
-    projects = Project.query.all()    
+    projects = Project.query.all()
     return render_template('index.html', projects=projects)
 
 
@@ -29,3 +31,8 @@ def robots():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
